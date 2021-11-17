30d334348e6cd1f21dbe2813ce93ad99629e6179
raghavsharma677 <81424060+raghavsharma677@users.noreply.github.com>
2021-11-17 17:10:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
 function setup() {
     video = createCapture(VIDEO);
-    video.size(150, 150);
+    video.size(550, 500);
 
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
@@ -10,16 +15,29 @@ function setup() {
 }
 
 function draw() {
+    document.getElementById("square_side").innerHTML = "Width And Height of a squair will be =" + difference +"px";
     background('#969A97');
+    fill('#F90093')
+    stroke('#F90093')
+    square(noseX, noseY, difference)
 }
 
 function modelLoaded() {
     console.log('poseNet is Initialized!')
 }
 
-function gotPoses() {
-    if(result.length > 0)
+function gotPoses(results) {
+    if(results.length > 0)
     {
-        console.log(result);
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX +" noseY =" + noseY); 
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX =" + leftWristX +" rightWristX =" + rightWristX + " difference =" + difference);
     }
 }
